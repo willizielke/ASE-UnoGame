87561d948ba911e82b28ab094a901fd21448b56e
Willi Zielke <92362551+willizielke@users.noreply.github.com>
2024-06-04 18:35:52 +0200

    Initial Commit

---
+++ UnoGame/src/main/java/ASE/UnoGame/App.java
@@ -0,0 +1,16 @@
+package ASE.UnoGame;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+    }
+}
