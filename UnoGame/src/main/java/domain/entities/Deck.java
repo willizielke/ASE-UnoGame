9acf7406bbcdb11b21ea4b25b7e86c091d82d9aa
Willi Zielke <92362551+willizielke@users.noreply.github.com>
2024-06-13 17:30:51 +0200

    Muster Builder added

---
+++ UnoGame/src/main/java/domain/entities/Deck.java
@@ -0,0 +1,13 @@
+package domain.entities;
+
+import java.util.List;
+
+public class Deck {
+    private List<Card> cards;
+
+    public Deck(List<Card> cards) {
+        this.cards = cards;
+    }
+}
+
+
---
+++ UnoGame/src/main/java/domain/entities/DeckBuilder.java
@@ -0,0 +1,66 @@
+package domain.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.GlobalConstants;
+
+public class DeckBuilder {
+    private List<Card> cards;
+
+    public DeckBuilder() {
+        this.cards = new ArrayList<>();
+    }
+
+    public DeckBuilder addNumberCard(String color, int number) {
+        cards.add(new NumberCard(color, number));
+        return this;
+    }
+
+    public DeckBuilder addSpecialCard(String color, String symbol) {
+        cards.add(new SpecialCard(color, symbol));
+        return this;
+    }
+
+    public DeckBuilder addWishCard() {
+        cards.add(new WishCard());
+        return this;
+    }
+
+    public DeckBuilder addPlus4Card() {
+        cards.add(new Plus4Card());
+        return this;
+    }
+
+    public Deck build() {
+        return new Deck(cards);
+    }
+
+    public static Deck createDeck() {
+        DeckBuilder deckBuilder = new DeckBuilder();
+        for (String color : GlobalConstants.COLORS) {
+            for (int number = 1; number <= 9; number++) {
+                deckBuilder.addNumberCard(color, number);
+                deckBuilder.addNumberCard(color, number);
+            }
+            deckBuilder.addSpecialCard(color, GlobalConstants.ZERO)
+                    .addSpecialCard(color, GlobalConstants.ZERO)
+                    .addSpecialCard(color, GlobalConstants.SKIP)
+                    .addSpecialCard(color, GlobalConstants.SKIP)
+                    .addSpecialCard(color, GlobalConstants.REVERSE)
+                    .addSpecialCard(color, GlobalConstants.REVERSE)
+                    .addSpecialCard(color, GlobalConstants.PLUS2)
+                    .addSpecialCard(color, GlobalConstants.PLUS2);
+        }
+        deckBuilder.addWishCard()
+                .addWishCard()
+                .addWishCard()
+                .addWishCard()
+                .addPlus4Card()
+                .addPlus4Card()
+                .addPlus4Card()
+                .addPlus4Card();
+
+        return deckBuilder.build();
+    }
+}
